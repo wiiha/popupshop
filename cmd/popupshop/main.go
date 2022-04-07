02b4c0d74f14fe7d8d96804066dc4197adc40f44
Wilhelm Hansson <wilhelm.hansson@pm.me>
2022-04-07 21:47:50 +0200

    ADD help flag

--- cmd/popupshop/main.go
+++ cmd/popupshop/main.go
@@ -13,11 +13,17 @@ func main() {
 	/*
 		Read flags from command line
 	*/
+	help := flag.Bool("help", false, "Show help message")
 	addr := flag.String("addr", "localhost", "Address to listen on")
 	port := flag.String("port", "8080", "Port to listen on")
 	dirToServe := flag.String("root", ".", "Root directory for files")
 
 	flag.Parse()
+
+	if *help {
+		flag.PrintDefaults()
+		return
+	}
 
 	fileService := files.NewFileService(*dirToServe)
 
